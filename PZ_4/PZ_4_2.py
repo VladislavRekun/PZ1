a3cfd5977548902f0695c97d4c3695de3e82e77a
VladislavRekun <vladislavrekun@gmail.com>
2023-11-29 18:34:22 +0300

    Initial commit

---
+++ PZ_4/PZ_4_2.py
@@ -0,0 +1,18 @@
+# Вторая задача
+# Дано целое число N (N > 0). Если оно является степенью число 3, то вывести TRUE, если не является - вывести FALSE.
+while True:
+    try:
+        n = int(input("Введите целое число n: "))
+        a = int(input("n = "))
+        sum = 0
+        term = 0
+    except ValueError:
+        print("Что-то не так")
+        continue
+    break
+
+st = 1
+
+while st < a : st = st*3
+
+print(st == n)
